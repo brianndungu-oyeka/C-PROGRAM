2c428bf9a9de252afa1b9328816a79c14fe60f84
brianndungu-oyeka <brianndungu866@gmail.com>
2026-09-15 10:39:20 +0300

    Units offered

---
+++ hello_2.c
@@ -0,0 +1,28 @@
+//program to display Units Offered In this Semester
+
+/*
+(Multiline Comment)
+Author:Brian Ndung'u Oyeka
+Registration number:BCS-05-0544/2026
+Description:Unit offered in this Semester program
+Date:10/09/2026
+Version 1
+*/
+
+//preprocessor directive
+#include <stdio.h> // printf()
+#include <math.h> tan()
+#define PI 3.142
+
+int main(){
+    //display units offered in this semester
+    printf("Computational Logic\n");
+    printf("Digital Literacy \n");
+    printf("Structured Programming and Algorithims \n");
+    printf("Basic Mathematics \n");
+    printf("Introduction to Physics \n");
+    printf("Communication Skills \n");
+    printf("Health and Wellness Management \n");
+    
+    return 0;
+    }
